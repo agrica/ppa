8237dfc33f2943e031151317bcde320002504b97
MORILLE Jérôme <morille.jerome@groupagrica.com>
2020-08-21 13:32:37 +0200

    Doc

--- ossRelease.sh
+++ ossRelease.sh
@@ -10,3 +10,9 @@ git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
 git checkout master
 git branch -D prepare-release
+
+# Not work pour 0-M3
+git merge ossrh-$VERSION
+mvn org.codehaus.mojo:versions-maven-plugin:2.8.1:set -DgenerateBackupPoms=false -DnextSnapshot=true
+git commit -am "Prepare Next Snapshot"
+git push origin master
