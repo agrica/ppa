f809a57bdea867355691f7df3e401c0ab74c6f6a
MORILLE Jerome <morille.jerome@groupagrica.com>
2021-11-22 10:45:25 +0100

    Add Release log

--- ossRelease.sh
+++ ossRelease.sh
@@ -14,12 +14,15 @@ echo "3- Create Maven Tag $VERSION"
 git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
 
-echo "Delete branch 'prepare-release'"
+echo "4- Delete branch 'prepare-release'"
 git checkout master
 git branch -D prepare-release
 
 # Not work pour 0-M3
+echo "5- Merge with 'master'"
 git merge ossrh-$VERSION
+
+echo "6- Increment Version 'master'"
 mvn org.codehaus.mojo:versions-maven-plugin:2.8.1:set -DgenerateBackupPoms=false -DnextSnapshot=true
 git commit -am "Prepare Next Snapshot"
 git push origin master
