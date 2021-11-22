2598c0f62ce1521c78dec3b891f116a08d30f335
MORILLE Jerome <morille.jerome@groupagrica.com>
2021-11-22 10:43:05 +0100

    Add Release log

--- ossRelease.sh
+++ ossRelease.sh
@@ -2,12 +2,19 @@
 
 VERSION=${1:-0.0.1-SNAPSHOT}
 
+echo "1- Create branch 'prepare-release'"
 git co -b prepare-release
+
+echo "2- Set Maven Version $VERSION"
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$VERSION
 mvn clean install
 git commit -am "Prépare Release version $VERSION"
+
+echo "3- Create Maven Tag $VERSION"
 git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
+
+echo "Delete branch 'prepare-release'"
 git checkout master
 git branch -D prepare-release
 
