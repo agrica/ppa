fec6b2060e0cca7a2eccb36853dd7ceb47d058cc
MORILLE Jerome <morille.jerome@groupagrica.com>
2022-09-05 13:51:27 +0200

    Modify script

--- ossRelease.sh
+++ ossRelease.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+BRANCH_NAME="master"
 VERSION=${1:-0.0.1-SNAPSHOT}
 if [[ -z "${VERSION}" ]]; then
     read -p "Version à releaser ? " VERSION
@@ -18,7 +19,7 @@ git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
 
 echo "4- Delete branch 'prepare-release'"
-git checkout master
+git checkout ${BRANCH_NAME}
 git branch -D prepare-release
 
 # Not work pour 0-M3
@@ -28,4 +29,4 @@ git merge ossrh-$VERSION
 echo "6- Increment Version 'master'"
 mvn org.codehaus.mojo:versions-maven-plugin:2.10.0:set -DgenerateBackupPoms=false -DnextSnapshot=true || echo "  versions:set $VERSION  ==> exit code $?"
 git commit -am "Prepare Next Snapshot"
-git push origin master
+git push origin ${BRANCH_NAME}
